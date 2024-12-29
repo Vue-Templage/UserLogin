d972f05416d0f04b5ddd158e5bf48ea9daedce88
Wangshch <wangshunc@126.com>
2024-12-29 16:23:16 +0800

    feat: 引入 layui-vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Layui from '@layui/layui-vue'
+import '@layui/layui-vue/lib/index.css'
 import router from '@/config/router'
 import axios from '@/config/axios'
 
 const app = createApp(App);
 app.provide("axios",axios);
+
 app.use(router)
+    .use(Layui)
     .mount('#app')
